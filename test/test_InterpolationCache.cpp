36e622edd107f779056464b575c03f9bf35e78e7
Willem Van Lishout <willemvanlishout@gmail.com>
2025-03-30 22:58:34 +0200

    add interpolation cache tests

--- test/test_InterpolationCache.cpp
+++ test/test_InterpolationCache.cpp
@@ -1,9 +1,16 @@
 #include "InterpolationCache.h"
 #include "doctest.h"
 
-TEST_CASE("Interpolation") {
+TEST_CASE("InterpolationCache") {
   auto cache = InterpolationCache::current();
   CHECK_EQ(0, cache.size());
   auto c = cache.get(Color::Red, Color::Green, 12);
   CHECK_EQ(1, cache.size());
+  CHECK_EQ(1, cache.cacheMisses());
+  auto col = Color::Red;
+  auto interp = (col.interpolate(Color::Green, 12), c);
+  CHECK_EQ(interp.size(), c.size());
+  c = cache.get(Color::Red, Color::Green, 12);
+  CHECK_EQ(1, cache.cacheMisses());
+  CHECK_EQ(c, interp);
 }
